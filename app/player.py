103dbc00285bab28f9a83d61b8bd6e13516041a8
Marc Cathomen <marc.cathomen@gmail.com>
2019-09-09 21:06:58 +0200

    refartoring, outsourced player class into player.py

---
+++ app/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.hand = []
+
+    def show(self):
+        print("id:{} name:{}".format(
+            self.id, self.name))
--- app/troccas.py
+++ app/troccas.py
@@ -1,19 +1,9 @@
 from deck import Card
 from deck import Deck
+from player import Player
 
 
 print("Hello World of Troccas")
-
-
-class Player:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.hand = []
-
-    def show(self):
-        print("id:{} name:{}".format(
-            self.id, self.name))
 
 
 class Game:
