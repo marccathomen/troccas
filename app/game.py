793f9cd34117404e18ad3804037b4ad7306eb6cd
Marc Cathomen <marc.cathomen@gmail.com>
2019-09-09 21:21:37 +0200

    refactoring  outsourcing Game class into game.py

---
+++ app/game.py
@@ -0,0 +1,16 @@
+from deck import Card
+from deck import Deck
+from player import Player
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.players = []
+        i = 0
+        for i in range(1, 5):
+            self.players.append(Player(i, "Player " + str(i)))
+
+    def show_players(self):
+        for p in self.players:
+            p.show()
--- app/troccas.py
+++ app/troccas.py
@@ -1,22 +1,8 @@
-from deck import Card
-from deck import Deck
-from player import Player
+
+from game import Game
 
 
 print("Hello World of Troccas")
-
-
-class Game:
-    def __init__(self):
-        self.deck = Deck()
-        self.players = []
-        i = 0
-        for i in range(1, 5):
-            self.players.append(Player(i, "Player " + str(i)))
-
-    def show_players(self):
-        for p in self.players:
-            p.show()
 
 
 game1 = Game()
