372aef89dfe55eb8cede33981fc5d8ea0768f98a
Marc Cathomen <marc.cathomen@gmail.com>
2019-09-10 20:06:42 +0200

    outsourcing Cards class in its own file

--- app/deck.py
+++ app/deck.py
@@ -1,16 +1,4 @@
-class Card:
-    def __init__(self, id, power, value, suit, label):
-        self.id = id  # unique identification number
-        self.power = power  # beating power
-        # value of the card in points
-        self.value = value
-        # suit of the card [r osa,s pada,c uppa,b astun,t rocca,n arr]
-        self.suit = suit
-        # label as on the real card [1,2,3,...,9,10,b uod, c cavagl, f emna, r etg, n arr, 1,2,...,20,21]
-        self.label = label
-
-    def show(self):
-        print(self.label, self.suit)
+from card import Card
 
 
 class Deck:
