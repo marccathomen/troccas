d242a8302e4f5f5ce3bbe3f76ee0b670dfd1c8f6
Marc Cathomen <marc.cathomen@gmail.com>
2019-09-10 18:42:33 +0200

    start deal_all

--- app/dealer.py
+++ app/dealer.py
@@ -6,8 +6,17 @@ class Dealer:
         self.deck = deck
         self.players = players
 
-    def deal_6(self):
-        pass
+    def deal_all(self):
+        p = []
+        for p in self.players:
+            '''
+            for i in range(0, 3):
+                for j in range(0, 7):
+                    p[i].hand.append(self.deck.draw_card()
+            '''
+        # last 2 cards go to last player
+        # p[i].hand.append(self.deck.draw_card()
+        # p[i].hand.append(self.deck.draw_card()
 
     def full_shuffle(self):
         for i in range(len(self.deck.cards) - 1, 0, -1):
