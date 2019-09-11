b79be79c54c99337611abdb46a98eea49e3ab176
Marc Cathomen <marc.cathomen@gmail.com>
2019-09-11 16:00:07 +0200

    add pile

--- app/player.py
+++ app/player.py
@@ -4,6 +4,7 @@ class Player:
         self.name = name
         self.hand = []
         self.points = 0
+        self.pile = []  # the cards that this plays won during the game
 
     def show(self):
         print("--------")
@@ -13,10 +14,17 @@ class Player:
         print("--------")
         for h in self.hand:
             h.show()
+        print("My pile")
+        
+        for p in self.pile:
+            p.show()
 
     def fai_scart(self):  # just remove the first 2 elements for now
-        self.hand.pop()
-        self.hand.pop()
+        self.pile.append(self.hand.pop())
+        self.pile.append(self.hand.pop())
 
     def add_points(self, points):
         self.points + points
+
+    def play_card(self, table):
+        return self.hand.pop()  # just play the first card in the hand
